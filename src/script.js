3b62ed3fe5db4317f10aa8f934c7384d6e025e10
filippeantonio@mail.com <filippeantonio@mail.com>
2025-09-07 22:35:21 +0200

    lämna in

--- src/script.js
+++ src/script.js
@@ -26,20 +26,27 @@ const contactBook = {
 };
   
   // Ange en grupp
-  let groupToShow;
+  let groupToShow = "familj";
   
 
 
 
-function showContacts(contactBook, groupToShow) { // Rör ej denna kod
-
-
+function showContacts(contactBook, groupToShow) { 
+  
+  // Rör ej denna kod
 
     // Loopar igenom alla kontakter och jämför grupp
+
+    for (contact of contactBook.contacts) {
+        if (contact.group === groupToShow) {
+            console.log(`${contact.name}: ${contact.phone}`);
    
-    return  // här ska sträng med namn och tekefonnummer visas i form av string interpolation
-
-
+    return `Namn: ${contact.name}, Telefon: ${contact.phone}`;
+    
+    // här ska sträng med namn och tekefonnummer visas i form av string interpolation
+    
+}
+}
 }
 
 
